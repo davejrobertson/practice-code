fc09476bd428d00aa395ba208539798aa2532db8
David Robertson <davejrobertson@users.noreply.github.com>
2015-12-15 18:50:33 +0000

    Incorporated journey distance/time
    Added directions service for journey distance and journey time.
    
    Improved marker feature.

--- angular/googlemaps/js/appMapping.js
+++ angular/googlemaps/js/appMapping.js
@@ -5,58 +5,208 @@ app.controller('mapCtrl', function($window, $scope, $http) {
 	$scope.gmaps = {
 			map: undefined,
 			geocoder: undefined,
+			director: undefined,
+			bounds: undefined,
+			startAddressDraft: undefined,
 			startAddress: undefined,
+			clickBtnStartAddress: function() {
+				$scope.gmaps.startAddress = $scope.gmaps.startAddressDraft;
+
+				$scope.gmaps.getMultiDirections($scope.customers);
+			},
+			getMultiDirections: function(customers) {
+				var customers = $scope.customers;
+
+				for (i=0; i<customers.length; i++) {
+					$scope.customers[i].journeyDistance = "loading";
+					$scope.customers[i].journeyTime = "loading";
+					$scope.gmaps.getDirections(customers[i], i);
+				}
+			},
+			getDirections: function(customer, index) {
+					//trigger each marker request sequentially after a timeout to avoid exceeding Google query limit
+					setTimeout(function(){
+
+						var address = customer.City + ", " + customer.Country;
+						var startAddress = $scope.gmaps.startAddress;
+						
+
+						//trigger re-rendering of the view
+						$scope.$digest();
+
+						var DirectionsRequest = {
+								origin: startAddress,
+								destination: address,
+								travelMode: google.maps.TravelMode.DRIVING
+							};
+
+						$scope.gmaps.director.route(DirectionsRequest, function(response, status) {
+								var customers = $scope.customers;
+								var custAddress = undefined;
+								var totalDistance = 0;
+								var totalDuration = "";
+								var legs = undefined;
+
+								//if successful, then
+								if (status === google.maps.DirectionsStatus.OK) {
+
+									//set the display values to total distance / duration
+									legs = response.routes[0].legs;
+
+									for(i=0; i<legs.length; i++) {
+									    totalDistance += legs[i].distance.value;
+									    totalDuration += legs[i].duration.text;
+									}
+
+									//not displaying directions right now
+									//directionsDisplay.setDirections(response);
+								} else {
+									//set the value to the status
+									totalDistance = status;
+									totalDuration = status;
+								}
+
+								//locate all applicable customer rows and apply the display value
+								for (i=0; i<customers.length; i++) {
+									custAddress = customers[i].City + ", " + customers[i].Country;
+
+									if (custAddress === response.request.destination) {
+										$scope.customers[i].journeyDistance = totalDistance;
+										$scope.customers[i].journeyTime = totalDuration;
+									}
+								}
+
+								//trigger re-rendering of the view
+								$scope.$digest();
+							});
+
+					}, 500 * index);
+
+					return undefined;
+			},
+			addMultiMarkers: function(customers) {
+				for (i=0; i<customers.length; i++) {
+					$scope.gmaps.addMarker(customers[i], i);
+				}
+			},
 			addMarker: function(customer, index) {
 
-					if (index < 5) {
+					//trigger each marker request sequentially after a timeout to avoid exceeding Google query limit
+					setTimeout(function(){
+
 						var address = customer.City + ", " + customer.Country;
 
-						this.geocoder.geocode( { 'address': address}, function(results, status) {
+						$scope.gmaps.geocoder.geocode( { 'address': address}, function(results, status) {
 								var map = $scope.gmaps.map;
+								var bounds = $scope.gmaps.bounds;
+
+								var latLng = results[0].geometry.location;
+
 								//If geocoded successfully, then add a marker
 								if (status == google.maps.GeocoderStatus.OK) {
 
-									map.setCenter(results[0].geometry.location);
+									//extend the bounds and center around the intended marker location
+									bounds.extend(latLng);
+									map.fitBounds(bounds);
 
+									//add the marker to the map
 									var marker = new google.maps.Marker({
 										map: map,
-										position: results[0].geometry.location
+										position: latLng
 									});
 
+									$scope.customers[index].markerIndex = index;
+									$scope.$digest();
 								}
 								else {
 									alert('error: ' + status);
 								}
 
 							});
-					}
+					}, 500 * index);
 
-					return index;
-				},
-			test: function() {
-				return 1;
-			}
+					return undefined;
+				}
 		};
 
 	$window.initMap = function() {
 
 			var gmaps = $scope.gmaps
 
+			//initialising the address > coordinate service
 			gmaps.geocoder = new google.maps.Geocoder();
 
+			gmaps.director = new google.maps.DirectionsService();
+
+			//initialising the map object
 			gmaps.map = new google.maps.Map(document.getElementById('map'), {
 					center: {lat: -34.397, lng: 150.644},
-					zoom: 1
+					zoom: 5
 				});
 
-			$http.get("http://www.w3schools.com/angular/customers.php").then(function (response) {
-					$scope.customers = response.data.records;
+			//this makes zooming/centering easier when adding markers
+			gmaps.bounds = new google.maps.LatLngBounds();
 
-					for (cust in $scope.customers) {
-						cust.lat
+
+			//fetch some data. . address hardcoded as only a demo
+			$http.get("http://www.w3schools.com/angular/customers.php").then(function (response) {
+					
+					var customers = response.data.records;
+					for (i=0; i<customers.length; i++) {
+						customers[i].markerIndex = "";
+						customers[i].journeyDistance = "";
+						customers[i].journeyTime = "";
 					}
+
+					//add the data to the scope
+					$scope.customers = customers;
+
+					//add all customers to map
+					gmaps.addMultiMarkers(customers);
+
 				});
 
 		};
 
+});
+
+app.filter('gMapsDistance', function() {
+  return function(input) {
+			var html;
+			if (input === "loading"){
+				html = "...";
+			}
+			else if (input === "ZERO_RESULTS") {
+				html = "X";
+			}
+			else if (input === "") {
+				html = "";
+			}
+			else {
+				html = "" + input / 1000 + " km";
+			}
+
+			return html;
+		};
+});
+
+app.filter('gMapsTime', function() {
+  return function(input) {
+			var html;
+			
+			if (input === "loading"){
+				html = "...";
+			}
+			else if (input === "ZERO_RESULTS") {
+				html = "X";
+			}
+			else if (input === "") {
+				html = "";
+			}
+			else {
+				html = input;
+			}
+
+			return html;
+		};
 });
